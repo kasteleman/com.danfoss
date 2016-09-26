85ab188280f33a8c466529aa074aae24e2cea31f
kasteleman <kasteleman@gmail.com>
2016-09-26 20:39:45 +0200

    setpoint_get

--- drivers/LC/driver.js
+++ drivers/LC/driver.js
@@ -1,42 +1,37 @@
-"use strict";
+'use strict';
 
-const ZwaveDriver = require('homey-zwavedriver');
-// http://www.pepper1.net/zwavedb/device/6
+const path			= require('path');
+const ZwaveDriver	= require('homey-zwavedriver');
 
-module.exports = new ZwaveDriver('LC', {
-    debug: true, // set to true to view all incoming events
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	debug: true,
     capabilities: {
-      'target_temperature': {
-  			'command_class'				: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
-        'command_get'    : 'THERMOSTAT_SETPOINT_GET',
-        'command_get_parser'		: function(){
-          console.log('Get');
-          return  {
-            'Level': {
-                    'Setpoint Type': 'Heating 1' // (or 'Cooling 1', are more options but seem superfluous)
-                }/*,
-                'Level2': {
-                    'Size': 0,
-                    'Scale': 0,
-                    'Precision': 0
-                }*/
-            //'Value': value
-            }
-          }
+      target_temperature: {
+        command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
+        command_get: 'THERMOSTAT_SETPOINT_GET',
+        command_get_parser: function () {
+                return {
+                    Level: {
+                        'Setpoint Type': 'Heating 1',
+                    },
+                };
+        },
+        command_report: 'THERMOSTAT_SETPOINT_REPORT',
+        command_report_parser: report => {
+                if (report.hasOwnProperty('Level2')
+                && report.Level2.hasOwnProperty('Scale')
+                && report.Level2['Scale'] === 0) {
+                    return report['Value (Parsed)'];
+                } return null;
+        },
       },
-      'measure_battery': {
-  			'command_class'				: 'COMMAND_CLASS_BATTERY',
-  			'command_report'    : 'BATTERY_REPORT',
+
+      measure_battery: {
+  			command_class: 'COMMAND_CLASS_BATTERY',
+        command_get: 'BATTERY_GET',
+  			command_report: 'BATTERY_REPORT',
         command_report_parser:  report => {
           console.log('REPORT');
-          console.log(report);
-          console.log(report['Battery Level (Raw)']);
-          console.log(report['Battery Level (Raw)'][0]);
-          return report['Battery Level (Raw)'][0];
-        },
-        'command_get'    : 'BATTERY_GET',
-        command_report_parser:  report => {
-          console.log('GET');
           console.log(report);
           console.log(report['Battery Level (Raw)']);
           console.log(report['Battery Level (Raw)'][0]);
@@ -47,4 +42,4 @@ module.exports = new ZwaveDriver('LC', {
 
 		settings: {
 		}
-	})
+	});
